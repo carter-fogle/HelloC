9a1cdf68172ccf3460a92288a2935d59edd01391
carter-fogle <111514967+carter-fogle@users.noreply.github.com>
2022-08-27 20:17:32 +0000

    inital commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * author Carter Fogle
+ *
+ * 2022/08/27
+ *
+ **/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int args, char **argv) {
+
+    printf("Hello World!\n");
+    printf("My name is Carter Fogle\nMy major is Computer Engineering\n");
+
+    return 0;
+}
